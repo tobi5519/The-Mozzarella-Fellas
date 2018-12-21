6ea8f900f3731568a26c3b99dde2fa0176e0af25
Andreas <rednex702@hotmail.com>
2018-12-21 13:50:46 +0100

    added pizza model

---
+++ The Mozzarella Fellas/Migrations/201812211245576_added pizza model.cs
@@ -0,0 +1,16 @@
+namespace The_Mozzarella_Fellas.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedpizzamodel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ The Mozzarella Fellas/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace The_Mozzarella_Fellas.Models
+{
+    public class Pizza
+    {
+        public int Id {get; set; }
+        public string Name { get; set; }
+        public string[] Toppings { get; set; }
+        // String = type eg family, normal ect and int = price
+        public Dictionary<string, int> Type { get; set; }
+
+
+    }
+}
